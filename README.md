14b90030dbe111c5ed3f19957fdc650d30db303f
Joyee Cheung <joyeec9h3@gmail.com>
2015-07-15 07:57:56 +0800

    new structure

---
+++ README.md
@@ -0,0 +1,69 @@
+## Dependencies
+
+These scripts need python 2.7+ and the following libraries to work:
+
+1. pillow(~2.8.1)
+2. numpy(~1.9.0)
+3. python-opencv(~2.4.11)
+4. scikit-learn (~0.15.2)
+The simplest way to install all of them is to install [python(x,y)](https://code.google.com/p/pythonxy/wiki/Downloads?tm=2).
+
+If you can't install python(x,y), You can install python, numpy and python-opencv seperately, then install pip and pillow.
+
+1. Install python. Just use the installer from [python's website](https://www.python.org/downloads/)
+2. Install numpy. Just use the installer from [scipy's website](http://www.scipy.org/scipylib/download.html). (You don't need scipy to run this project, so you can just install numpy alone).
+3. Install python-opencv. Download the release from [its sourceforge site](http://sourceforge.net/projects/opencvlibrary/files/). (Choose the release based on your operating system, then choose version 2.4.11). The executable is just an archive. Extract the files, then copy `cv2.pyd` to the `lib/site-packages` folder on your python installation path.
+4. Install pip. Download [the script for installing pip](https://bootstrap.pypa.io/get-pip.py), open cmd (or termianl if you are using Linux/Mac OS X), go to the path where the downloaded script resides, and run `python get-pip.py`
+5. Install pillow. Run `pip install pillow`. 
+6. Install scikit-learn. Run `pip install scikit-learn`
+
+If you are running the code under Linux/Mac OS X and the scripts throw `AttributeError: __float__`, make sure your pillow has jpeg support (consult [Pillow's document](http://pillow.readthedocs.org/en/latest/installation.html)) e.g. try:
+
+```
+sudo apt-get install libjpeg-dev
+sudo pip uninstall pillow
+sudo pip install pillow
+```
+
+If you have any problem installing the dependencies, contact the author.
+
+## How to generate the results
+
+Enter the `src` directory, run
+
+```
+python main.py
+```
+
+It will use images(`.jpg` only) under `test` directory to produce the results. The results will show up in `results` directory. Results generated with OpenCV will have `-cv` in its filename and results generated with sklearn will have `-sk` in its filename.
+
+
+##Directory structure
+
+```
+.
+├─ README.md
+├─ doc (documentations, reports)
+│   └── report.pdf
+├─ classifier (OpenCV cascade classifier)
+│   ├── cascade.xml (the classifier parameter file)
+│   └── ...
+├─ MNIST (The MNIST database)
+│   ├── train-images.idx3-ubyte
+│   └── train-labels.idx1-ubyte
+├─ test (test images)
+│   └── ...
+├─ results (the results)
+│   └── ...
+└─ src (the python source code)
+    ├── detect.py (detection code)
+    ├── load_labels.py (script to load MNIST data)
+    ├── recognize.py (recognition code)
+    └── main.py (generate the results)
+```
+
+##About
+
+* [Github repository](https://github.com/joyeecheung/perspective-correction)
+* Author: Qiuyi Zhang
+* Time: Apr. 2015
--- src/main.py
+++ src/main.py
@@ -9,16 +9,17 @@ from detect import detect, crop_detection, annotate_detection
 from load_labels import get_data
 from recognize import cvtrain, sktrain, preprocess
 from recognize import annotate_recognition
+from glob import glob
+import os
 
 SAMPLE_SIZE = (28, 28)
 SZ = 28
 LABEL_FILE = '../MNIST/train-labels.idx1-ubyte'
 IMAGE_FILE = '../MNIST/train-images.idx3-ubyte'
-CASCADE_FILE = '../asset/classifier/cascade.xml'
-TEST_FILE = '../asset/test/8.jpg'
+CASCADE_FILE = '../classifier/cascade.xml'
+TEST_FILES = '../test/'
+RESULT_FILES = '../results/'
 
-RESULT_FILE_CV = '../asset/results/cv.jpg'
-RESULT_FILE_SK = '../asset/results/sk.jpg'
 FONT_FILE = 'arial.ttf'
 FONT_SIZE = 30
 TEST_FONT = '5'
@@ -33,36 +34,44 @@ affine_flags = cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR
 
 
 def main():
-    img = cv2.imread(TEST_FILE)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    im = Image.open(TEST_FILE)
-    digits = detect(gray, CASCADE_FILE)
-    results = crop_detection(im.copy(), digits)
-    test = [np.float32(i.resize(SAMPLE_SIZE)).ravel() for i in results]
-
     images, labels, num, rows, cols = get_data(LABEL_FILE,
                                                IMAGE_FILE)
-
-    font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
-    detected = annotate_detection(im.copy(), digits)
-
     print 'Training OpenCV SVM...'
     svc1 = cvtrain(images[:TRAIN_SIZE], labels[:TRAIN_SIZE], num, rows, cols)
-    traindata = preprocess(test, rows, cols).reshape(-1, bin_n * 4)
-    yhat1 = svc1.predict_all(traindata)
-    yhat1 = yhat1.astype(np.uint8).ravel()
-
-    recognized = annotate_recognition(detected, digits, yhat1, font)
-    recognized.show()
-    recognized.save(RESULT_FILE_CV)
 
     print 'Training sklearn SVM...'
     svc2 = sktrain(images[:TRAIN_SIZE], labels[:TRAIN_SIZE])
-    yhat2 = svc2.predict(test)
 
-    recognized = annotate_recognition(detected, digits, yhat2, font)
-    recognized.show()
-    recognized.save(RESULT_FILE_SK)
+    filenames = glob(TEST_FILES + "/*.jpg")
+    for filename in filenames:
+        print 'Processing', filename
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        im = Image.open(filename)
+        digits = detect(gray, CASCADE_FILE)
+        results = crop_detection(im.copy(), digits)
+        test = [np.float32(i.resize(SAMPLE_SIZE)).ravel() for i in results]
+
+        testdata = preprocess(test, rows, cols).reshape(-1, bin_n * 4)
+        yhat1 = svc1.predict_all(testdata)
+        yhat1 = yhat1.astype(np.uint8).ravel()
+        yhat2 = svc2.predict(test)
+
+        font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
+        detected = annotate_detection(im.copy(), digits)
+
+        basename = os.path.basename(filename)
+        resultname = RESULT_FILES + '/' + basename
+
+        print 'OpenCV results'
+        recognized = annotate_recognition(detected, digits, yhat1, font)
+        recognized.show()
+        recognized.save(resultname.replace('.jpg', '-cv.jpg'))
+
+        print 'sklearn results'
+        recognized = annotate_recognition(detected, digits, yhat2, font)
+        recognized.show()
+        recognized.save(resultname.replace('.jpg', '-sk.jpg'))
 
 
 if __name__ == '__main__':
